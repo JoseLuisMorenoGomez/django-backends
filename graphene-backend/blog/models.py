c362692d90833a3cf499d266d37dd9af6fcae8cd
JoseLuisMorenoGomez <jlmoreno@ocmjerez.org>
2023-12-14 09:02:23 +0100

    kk

--- graphene-backend/blog/models.py
+++ graphene-backend/blog/models.py
@@ -6,30 +6,31 @@ from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from wagtail.search import index
 
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import StreamField, RichTextField
+from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
-
     content_panels = Page.content_panels + [
         FieldPanel('intro')
     ]
 
-
-
-# Keep the definition of BlogIndexPage model, and add the BlogPage model:
-
 class BlogPage(Page):
+    author = models.CharField(max_length=255, blank=True)
     date = models.DateField("Post date")
-    intro = models.CharField(max_length=250, default='intro')
-    body = RichTextField(blank=True)
-
-    search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
-    ]
-
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True)
     content_panels = Page.content_panels + [
+        FieldPanel('author'),
         FieldPanel('date'),
-        FieldPanel('intro'),
         FieldPanel('body'),
-    ]    
+    ]
     
